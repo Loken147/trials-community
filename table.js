5f212811ef2af416845820f52126b35fa2a56763
Loken147 <thomasjoveng@gmail.com>
2022-01-30 16:49:51 +0100

    Post

---
+++ table.js
@@ -0,0 +1,23 @@
+// Search Function
+const listItems = document.querySelectorAll('.list-group-item')
+
+document.querySelector('#search-input').
+    addEventListener('input', filterList)
+
+
+function filterList() {
+    const searchInput = document.querySelector
+        ('#search-input')
+    const filter = searchInput.value.toLowerCase()
+    const listItems = document.querySelectorAll('.list-group-item')
+
+    listItems.forEach((item) => {
+        let text = item.textContent;
+        if (text.toLowerCase().includes(filter.toLowerCase())) {
+            item.style.visibility = ''
+        }
+        else {
+            item.style.visibility = 'collapse';
+        }
+    });
+}
